0f6b4f0faef8c031e1c670fcc0bac8fac99b7c44
Inyin12 <163719249+Inyin12@users.noreply.github.com>
2024-03-17 11:21:47 +0300

    eclipse

---
+++ ttt/src/package1/TestClass1.java
@@ -0,0 +1,16 @@
+package package1;
+
+import java.util.Scanner;
+
+public class TestClass1 {
+	
+ public static void main(String[] args) {
+	 
+	 int agee;
+	 Scanner in = new Scanner(System.in);
+	 agee = in.nextInt();
+	 in.close();
+	 System.out.print(agee);
+ 
+ }
+}
